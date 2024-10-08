1a01a89e484477858dc0af01f5e53f83a3614488
Javeria-Shakoor9 <javeriashakoor9@gmail.com>
2024-10-08 21:05:24 +0500

    Add files via upload

---
+++ src/java/consult1.java
@@ -0,0 +1,58 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class consult1 extends HttpServlet {
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+//            String name = request.getParameter("name");
+//            String email = request.getParameter("email");
+//            String phone = request.getParameter("phno");
+            String reason = "x";//request.getParameter("reason");
+            String name = request.getParameter("day");
+            String timem = request.getParameter("timem");
+            String timet = request.getParameter("timet");
+                try {
+                    Class.forName("com.mysql.cj.jdbc.Driver");
+                    Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sys","root","root9@gmail.com");
+                    PreparedStatement stm = con.prepareStatement("Insert into webdb values('"+name+"','"+timem+"','"+timet+"')");
+                    int x = stm.executeUpdate();
+                    if(x>0){
+                        //response.sendRedirect("Consultation.html");
+                        out.println("Record inserted.");
+                    }
+                } catch (ClassNotFoundException | SQLException ex) {
+                    Logger.getLogger(consult1.class.getName()).log(Level.SEVERE, null, ex);
+                }   
+            }
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
